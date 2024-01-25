2375101a23c81b132089c8d77ded2015897b95f6
Ramadoss3427 <ramadoss3427@gmail.com>
2024-01-24 18:21:36 -0800

    task completed

---
+++ script.js
@@ -0,0 +1,34 @@
+const count = document.getElementById("no");
+const str = document.getElementById("str");
+
+setTimeout(()=>{count.innerHTML=10
+    setTimeout(()=>{count.innerHTML=9
+        setTimeout(()=>{count.innerHTML=8
+            setTimeout(()=>{count.innerHTML=7
+                setTimeout(()=>{count.innerHTML=6
+                    setTimeout(()=>{count.innerHTML=5
+                        setTimeout(()=>{count.innerHTML=4
+                            setTimeout(()=>{count.innerHTML=3
+                                setTimeout(()=>{count.innerHTML=2
+                                    setTimeout(()=>{count.innerHTML=1
+                                       
+                                            setTimeout(()=>{
+                                                count.innerHTML="";
+                                                str.innerHTML="Happy Independence Day";
+                    
+                                            },1000)
+                                      
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+}, 1000);
+setTimeout(()=> {
+    body. innerHTML = ""
+   body.URL("d.jpg")
+},10000)
